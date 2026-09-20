b18659db69efb87b98b2b10153452496ac22570a
Abhijeet Kasera <146567228+ABHIJEET-0001@users.noreply.github.com>
2026-09-20 23:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (50.03%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void dfs(TreeNode* root, string path) {
+
+        if(root == NULL) return;
+
+        // Current node add karo
+        if(path.empty())
+            path = to_string(root->val);
+        else
+            path += "->" + to_string(root->val);
+
+        // Leaf node
+        if(root->left == NULL && root->right == NULL){
+            ans.push_back(path);
+            return;
+        }
+
+        dfs(root->left, path);
+        dfs(root->right, path);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+
+        dfs(root, "");
+
+        return ans;
+    }
+};
